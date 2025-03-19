8f4b5da73962af294cefde13845f464556686e97
Chris Borchert <christopher.borchert@gmail.com>
2025-03-19 12:44:18 +0000

    put script in footer

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,10 @@
-(function onStart() {
+const myButton = document.getElementById('my-button');
+console.log("started! myButton", myButton)
 
-    const myButton = document.getElementById('my-button');
-    console.log("started! myButton", myButton)
+if (myButton) {
+    console.log('adding event listener to myButton')
+    myButton.addEventListener('click', () => {
+        alert("hello!");
+    });
+}
 
-    if (myButton) {
-        console.log('adding event listener to myButton')
-        myButton.addEventListener('click', () => {
-            alert("hello!");
-        });
-    }
-})()
